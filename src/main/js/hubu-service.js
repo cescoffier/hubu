b9d7fd796d136b2bc9c5c5e4468672c350d90037
Clement Escoffier <clement.escoffier@gmail.com>
2012-04-30 15:40:14 +0200

    Refactor pom + Modularization
    Start modularization effort
    Maven now aggregates all files and minify the resulting version
    The jasmine tests are run 3 times:
    * on the modular version
    * on the aggregated version
    * on the minified version

---
+++ src/main/js/hubu-service.js
@@ -0,0 +1,96 @@
+DE_AKQUINET.hubu.registry = {
+
+    services: [],
+    nextid: 0,
+
+
+    registerService: function(service, component, properties) {
+        // Increment the service.id
+        var id = this.currentid,
+            props = null,
+            reg = null;
+
+        this.nextid = this.nextid + 1;
+
+        if (properties === undefined  || properties === null) {
+            props = {}
+        } else {
+            props = properties;
+        }
+
+        props.id = id;
+        props.service = service;
+
+        reg = {
+            'contract': service,
+            'component': component,
+            'id': id,
+            'properties' : props
+        };
+
+        this.services.push(reg);
+
+        return reg;
+    },
+
+    unregisterService: function(reg) {
+        var index = this.findServiceIndexById(reg.id),
+            removedService = null;
+
+        if (index !== -1) {
+            removedService = this.services.splice(index, 1);
+        }
+
+        return removedService;
+    },
+
+    unregisterAllServicesFromComponent: function(component) {
+        var removedService = null,
+            i = 0,
+            indexes = [];
+
+        for (; i < this.services.length; i++) {
+            if (this.services[i].component === component) {
+                indexes.push(i);
+            }
+        }
+
+        for (i = indexes.length; i > 0; i--) {
+            this.services.splice(indexes[i], 1);
+        }
+
+        return removedService;
+    },
+
+    getServiceReferencesByContract: function(service) {
+        var svc = [];
+        var i = 0;
+        for (; i < this.services.length; i++) {
+            if (this.services[i].contract === service) {
+                svc.push(this.services[i]);
+            }
+        }
+        return svc;
+    },
+
+    findServiceById: function(id) {
+        var i = 0;
+        for (; i < this.services.length; i++) {
+            if (this.services[i].id === id) {
+                return this.service[i];
+            }
+        }
+        return null;
+    },
+
+    findServiceIndexById: function(id) {
+        var i = 0;
+        for (; i < this.services.length; i++) {
+            if (this.services[i].id === id) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
